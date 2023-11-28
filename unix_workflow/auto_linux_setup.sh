f42609dfd69c6354eab681d8a28ad08e6133fb11
Amine Djeghri <32715913+AmineDjeghri@users.noreply.github.com>
2023-11-28 01:10:57 +0100

    Update auto_linux_setup.sh

--- unix_workflow/auto_linux_setup.sh
+++ unix_workflow/auto_linux_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # colors
+GREEN='\e[32m'
 YELLOW='\e[93m'
 RED='\e[91m'
 RESET='\e[0m'
@@ -23,107 +24,140 @@ OS=$(uname)
 
 echo "Detected OS: $OS"
 
-# Ask user if they want to customize the terminal
-if ask_yes_no "Do you want to run sudo apt update & upgrade to load latest packages?"; then
-sudo apt update
-sudo apt upgrade
-fi
+# Function to update and upgrade packages
+update_upgrade_packages() {
+    if ask_yes_no "Do you want to run sudo apt update & upgrade to load the latest packages?"; then
+        sudo apt update
+        sudo apt upgrade
+    fi
+}
 
-# Ask user if they want to customize the terminal
-if ask_yes_no "Do you want to customize the terminal by installing ZSH, Oh My Zsh, and the Powerlevel10k theme?"; then
+# Function to install Zsh
+install_zsh() {
+    # Check if Zsh is already installed
+    if type zsh >/dev/null 2>&1; then
+        echo "${YELLOW} Zsh is already installed. ${RESET}"
+        return
+    fi
+
+    # Install Zsh
+    echo "Installing zsh..."
+    sudo apt install zsh
+
+    # Check zsh version
+    echo "Checking zsh version..."
+    zsh --version
+
+    # Restart the shell script
+    echo "Restarting shell script..."
+    source ~/.zshrc
+    echo "Current shell: $SHELL"
+    $SHELL --version
+}
+
+# Function to install Oh My Zsh, plugins & powerlevel10k theme
+install_oh_my_zsh_pl10k() {
+    # Check if Zsh is installed
+    if ! type zsh >/dev/null 2>&1; then
+        echo "${RED} Zsh is required for Oh My Zsh. Please install Zsh first. ${RESET}"
+        return
+    fi
+      echo "Current shell: $SHELL"
+      $SHELL --version
+
+      # Install Oh My Zsh
+      echo "Installing Oh My Zsh..."
+
+      # i changed the normal commande :
+      # "sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"" to the one below to
+      # avoid exiting the script after the installation
+      OH_MY_ZSH_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+      echo "Installing Oh My Zsh..."
+      wget "$OH_MY_ZSH_INSTALL_SCRIPT_URL" -O install_oh_my_zsh.sh
+      chmod +x install_oh_my_zsh.sh
+      ./install_oh_my_zsh.sh --unattended
+
+      # Install plugins
+      echo "Installing plugins..."
+      git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+      git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+      git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+
+      echo "Updating .zshrc configuration..."
+      curl -sL "https://raw.githubusercontent.com/AmineDjeghri/Awesome-Windows11-WSL-Linux/master/unix_workflow/.zshrc" > ~/.zshrc
+
+       # Check if Oh My Zsh is installed
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo "${RED} Oh My Zsh is required for Powerlevel10k theme. Please install Oh My Zsh first. ${RESET}"
+        return
+    fi
+
     # Check if the font "MesloLGS NF Regular.ttf" is installed
     if ask_yes_no "Is the font 'MesloLGS NF Regular.ttf' installed?"; then
-        # Install zsh
-        echo "Installing zsh..."
-        sudo apt install zsh
-
-        # Check zsh version
-        echo "Checking zsh version..."
-        zsh --version
-
-        # Restart the shell script
-        echo "Restarting shell script..."
-        source ~/.zshrc
-        echo "Current shell: $SHELL"
-        $SHELL --version
-
-        # Install Oh My Zsh
-        echo "Installing Oh My Zsh..."
-        sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-        # Install plugins
-        echo "Installing plugins..."
-        git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-
         # Install Powerlevel10k theme
         echo "Installing Powerlevel10k theme..."
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-        echo "Updating .zshrc configuration..."
-        curl -sL "https://raw.githubusercontent.com/AmineDjeghri/Awesome-Windows11-WSL-Linux/master/unix_workflow/.zshrc" > ~/.zshrc
         curl -sL "https://raw.githubusercontent.com/AmineDjeghri/Awesome-Windows11-WSL-Linux/master/unix_workflow/.p10k.zsh" > ~/.p10k.zsh
         source ~/.zshrc
-
-        # Print installation message
-        echo -e "\e[93m(Optional) If you have custom configurations in your .bashrc, consider copying them to the .zshrc file.\e[0m"
+        echo  "${YELLOW} Powerlevel10k has been automatically initialized in your terminal and you should see colors with icons. if not, run 'p10k configure' .${RESET}"
+        echo  "${YELLOW}(Optional) If you have custom configurations in your .bashrc, consider copying them to the .zshrc file.${RESET}"
 
     else
-        echo "\033[0;33mPlease install the font 'MesloLGS NF Regular.ttf' before customizing the terminal."
-        echo "You can check this link for instructions: https://github.com/AmineDjeghri/Auto-Linux-Setup/tree/main#1-setup-linux-automatically-\e[0m"
+        echo "${RED}Please install the font 'MesloLGS NF Regular.ttf' in your terminal then reinstall powerlevel10k again.${RESET}"
     fi
-else
-    echo "\e[93mTerminal customization skipped.\e[0m"
-fi
 
-# Check if Conda is already installed
-if command -v conda >/dev/null 2>&1; 
-then
-    echo "Conda is already installed."
-else
-  echo "Conda is not installed."
-  
-  case "$(uname -s)" in
-    Linux*)
-    # Download and install Miniconda
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    chmod +x Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh
+}
 
-    # Initialize Conda for the current shell
-    source $HOME/miniconda3/etc/profile.d/conda.sh
 
-    echo "${YELLOW}Initializing Conda for the default shell $(basename $SHELL) ${RESET}"
-    # Get the shell's configuration file
-    shell_config=""
+# Function to install miniconda
+install_miniconda3() {
+  # Check if Conda is already installed
+  if command -v conda >/dev/null 2>&1;
+  then
+      echo "${YELLOW} Conda is already installed. ${RESET}"
+  else
+    echo "${YELLOW} Conda is not installed. Installing... ${RESET}"
 
-    case "$(basename $SHELL)" in
-        bash) shell_config=~/.bashrc;;
-        zsh) shell_config=~/.zshrc;;
-        *)   echo "${RED}Shell type $(basename $SHELL) is not supported. Manual initialization may be required.${RESET}";;
-    esac
+    case "$(uname -s)" in
+      Linux*)
+      # Download and install Miniconda
+      wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+      chmod +x Miniconda3-latest-Linux-x86_64.sh
+      bash Miniconda3-latest-Linux-x86_64.sh
 
-    if [ -n "$shell_config" ]; then
-        # Initialize Conda for the restarted shell
-        ~/miniconda3/bin/conda init $(basename $SHELL)
+      # Initialize Conda for the current shell
+      source $HOME/miniconda3/etc/profile.d/conda.sh
 
-    echo -e "${YELLOW} Conda has been automatically initialized in your terminal and you should see (base). if not, restart it manually and verify that it's inside your file shell_config.${RESET}"
-  fi
-  ;;
-  *)
-    echo "${RED}Miniconda installation is only supported on Linux at the moment.${RESET}"
+      echo "${YELLOW}Initializing Conda for the default shell $(basename $SHELL) ${RESET}"
+      # Get the shell's configuration file
+      shell_config=""
+
+      case "$(basename $SHELL)" in
+          bash) shell_config=~/.bashrc;;
+          zsh) shell_config=~/.zshrc;;
+          *)   echo "${RED}Shell type $(basename $SHELL) is not supported. Manual initialization may be required.${RESET}";;
+      esac
+
+      if [ -n "$shell_config" ]; then
+          # Initialize Conda for the restarted shell
+          ~/miniconda3/bin/conda init $(basename $SHELL)
+
+      echo  "${YELLOW} Conda has been automatically initialized in your terminal and you should see (base). if not, restart it manually and verify that it's inside your file shell_config.${RESET}"
+    fi
     ;;
-  esac
-fi
+    *)
+      echo "${RED}Miniconda installation with this script is only supported on Linux at the moment.${RESET}"
+      ;;
+    esac
+  fi
+}
 
-
-# install nvidia driver
-if ask_yes_no "Do you want to install nvidia driver?"; then
-
+# Function to install nvidia driver
+install_nvidia_driver() {
   # Check if NVIDIA driver is installed
   if nvidia-smi >/dev/null 2>&1; then
-    echo "NVIDIA driver is already installed."
+    echo "${YELLOW} NVIDIA driver is already installed. ${RESET}"
     # Display GPU information
     echo "GPU Information:"
     nvidia-smi --query-gpu=name --format=csv,noheader
@@ -141,8 +175,8 @@ if ask_yes_no "Do you want to install nvidia driver?"; then
 
         # Make the downloaded file executable
         chmod +x NVIDIA-Linux-x86_64-535.129.03.run
-        # install gcc 
-        sudo apt install build-essential
+        # install gcc
+        sudo apt install buildssential
         # Run the NVIDIA driver installer
         sudo ./NVIDIA-Linux-x86_64-535.129.03.run
 
@@ -156,11 +190,90 @@ if ask_yes_no "Do you want to install nvidia driver?"; then
         nvidia-smi --query-gpu=name --format=csv,noheader
         nvidia-smi
     else
-    echo "Unsupported operating system: $OS"
+    echo "${RED} Unsupported operating system: $OS ${RESET}"
     fi
   fi
-fi
+
+}
+
+# Function to uninstall Zsh and remove Oh My Zsh and Powerlevel10k
+uninstall_zsh_omz_pl10k() {
+    # Check if Zsh is installed
+    if ! type zsh >/dev/null 2>&1; then
+        echo "Zsh is not installed."
+        return
+    fi
+
+    # Ask for confirmation
+    if ask_yes_no "${YELLOW} Do you want to uninstall Zsh and remove Oh My Zsh and Powerlevel10k? ${RESET} "; then
+        # Remove Oh My Zsh
+        if [ -d "$HOME/.oh-my-zsh" ]; then
+            echo "Removing Oh My Zsh..."
+            rm -rf "$HOME/.oh-my-zsh"
+            rm -rf "$HOME/.zshrc"
+        fi
+
+        # Remove .zshrc
+        if [ -f "$HOME/.zshrc" ]; then
+            echo "Removing .zshrc..."
+            rm "$HOME/.zshrc"
+        fi
+
+        # Remove Powerlevel10k theme
+        if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+            echo "Removing Powerlevel10k theme..."
+            rm -rf "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+        fi
+
+        # Uninstall Zsh
+        echo "Uninstalling Zsh..."
+        sudo apt remove zsh
+
+        echo "Zsh, Oh My Zsh, and Powerlevel10k have been uninstalled."
+    else
+        echo "Uninstallation canceled."
+    fi
+}
+
+
+
+# Function to show the menu
+show_menu() {
+  # Welcome message
+  echo "${GREEN}Welcome to the Auto Linux Setup. This script helps you set up automatically various tools on your system."
+  echo "It has been tested on Ubuntu 22.04. ${RESET}"
+  printf "\n"
+  echo "${YELLOW} ============ Menu ============ ${RESET}"
+  echo "${YELLOW} 1. Install ZSH ${RESET}"
+  echo "${YELLOW} 2. Install Oh My Zsh, plugins & Powerlevel10k theme ${RESET}"
+  echo "${YELLOW} 3. Install Minconda3 ${RESET}"
+  echo "${YELLOW} 4. Install NVIDIA driver ${RESET}"
+  echo "${YELLOW} 5. Uninstall ZSH, OMZ & Pl10K ${RESET}"
+  echo "${YELLOW} 0. Exit ${RESET}"
+  read -p "Enter your choice (1-5): " choice
+
+  case $choice in
+      1) install_zsh;;
+      2) install_oh_my_zsh_pl10k;;
+      3) install_miniconda3;;
+      4) install_nvidia_driver;;
+      5) uninstall_zsh_omz_pl10k;;
+      0) exit 0;;
+      *) echo "Invalid choice. Exiting..."; exit 1;;
+  esac
+
+}
+
+# Menu loop
+while true; do
+    show_menu
+    if ask_yes_no "Do you want to show the menu again?"; then
+        continue
+    else
+        break
+    fi
+done
 
 # restarting the terminal
-echo "${YELLOW}Restarting the terminal...${RESET}"
+echo "${YELLOW}Restarting the terminal... Please run the script again to continue installing other stuff ${RESET}"
 exec $SHELL
