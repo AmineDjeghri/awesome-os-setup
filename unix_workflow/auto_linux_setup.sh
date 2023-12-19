ba1ed39ff62accf1058cc89ec0eec776b7830ba1
Amine Djeghri <32715913+AmineDjeghri@users.noreply.github.com>
2023-12-19 14:59:34 +0100

    Update auto_linux_setup.sh

--- unix_workflow/auto_linux_setup.sh
+++ unix_workflow/auto_linux_setup.sh
@@ -82,7 +82,7 @@ install_oh_my_zsh_pl10k() {
       git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
       git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
       git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-
+      git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
       echo "Updating .zshrc configuration..."
       curl -sL "https://raw.githubusercontent.com/AmineDjeghri/Awesome-Windows11-WSL-Linux/master/unix_workflow/.zshrc" > ~/.zshrc
 
