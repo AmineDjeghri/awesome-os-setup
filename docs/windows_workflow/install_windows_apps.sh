95b1f93bf20883eee5f66255e1293e18232fbc26
Amine Djeghri <amine.djeghri1@gmail.com>
2024-01-10 00:40:11 +0100

    :memo: update docs

--- docs/windows_workflow/install_windows_apps.sh
+++ docs/windows_workflow/install_windows_apps.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
-echo 'This script can only be used in bash'
+# Sources come from : https://winstall.app/ & https://winget.run/ &  Windows store
+echo 'This script can only be used in bash.'
+echo 'Welcome to the Windows apps installer script. This script will INSTALL or UPDATE the following apps:'
 
 declare -A apps=(
     [11]="Brave.Brave"
     [12]="Mozilla.Firefox"
     [13]="Microsoft.Edge"
-    [21]="WhatsApp.WhatsApp"
-    [22]="Viber.Viber"
-    [23]="Telegram.TelegramDesktop"
-    [24]="Zoom.Zoom"
-    [25]="Discord.Discord"
+
+    [21]="Facebook.Messenger"
+    [22]="Discord.Discord"
+    [23]="WhatsApp.WhatsApp"
+    [24]="Viber.Viber"
+    [25]="Telegram.TelegramDesktop"
     [26]="Snapchat.Snapchat"
     [27]="Instagram.Instagram"
-    [31]="Docker.DockerDesktop"
-    [32]="JetBrains.IntelliJIDEA"
-    [33]="Microsoft.VisualStudioCode"
-    [34]="obsidianmd.obsidian"
-    [35]="SublimeHQ.SublimeText.4"
-    [41]="Apple.AppleMusic"
-    [42]="Spotify.Spotify"
+    [28]="Zoom.Zoom"
+    [29]="Microsoft.Teams"
+
+    [31]="VideoLAN.VLC"
+    [34]="Netflix"
+    [35]="Prime Video"
+    [36]="Disney+"
+
+    [41]="Spotify.Spotify"
+    [42]="Apple.AppleMusic"
+
     [51]="Valve.Steam"
     [52]="EpicGames.EpicGamesLauncher"
+    [53]="Nvidia.GeForceExperience"
+    [54]="Ubisoft.Connect"
+    [55]="SideQuestVR.SideQuest"
+
+    [61]="Microsoft 365"
+    [62]="Adobe Acrobat Reader DC"
+    [63]="Google.Drive"
+    [64]="Microsoft.OneDrive"
+    [65]="Notion.Notion"
+    [66]="SublimeHQ.SublimeText.4"
+    [67]="obsidianmd.obsidian"
+
+    [71]="OBSProject.OBSStudio"
+    [72]="Creative Cloud"
+    [74]="ByteDance.CapCut"
+    [75]="microsoft clipchamp"
+    [76]="canva.canva"
+
+
+    [81]="NordVPN.NordVPN"
+    [82]="qBittorrent.qBittorrent"
+    [83]="Ookla.Speedtest.Desktop"
+
+
+    [92]="Microsoft.WindowsTerminal"
+    [93]="Microsoft.VisualStudioCode"
+
+
+    [01]="Microsoft.BingWallpaper"
+    [02]="File-New-Project.EarTrumpet"
+    [02]="ShareX.ShareX"
 )
 
 declare -A categories=(
     [1]="Web Browsers"
     [2]="Messaging"
-    [3]="Development Tools"
-    [4]="Media and Entertainment"
+    [3]="Meetings and Conferencing"
+    [4]="Media, music and Entertainment"
     [5]="Gaming"
+    [6]="Documents, drives & editing"
+    [7]="Image & Video Editing"
+    [8]="Security & network tools"
+    [9]="Development & Programming"
+    [0]="Others"
 )
 
 display_menu() {
