91c8e23130a1f4a241c660049d25a2b2b348e605
Amine Djeghri <32715913+AmineDjeghri@users.noreply.github.com>
2024-04-20 00:59:08 +0200

    Update one_click_installer.sh

--- docs/unix_workflow/package_example/one_click_installer.sh
+++ docs/unix_workflow/package_example/one_click_installer.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # colors
 YELLOW='\e[93m'
 RED='\e[91m'
@@ -18,9 +16,48 @@ ask_yes_no() {
     done
 }
 
+# Function to parse arguments in key=value format otherwise prompt for input
+parse_arguments() {
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            env_name=*)
+                env_name="${1#*=}"
+                ;;
+            device=*)
+                device="${1#*=}"
+                ;;
+            *)
+                echo "Invalid argument: $1"
+                ;;
+        esac
+        shift
+    done
+
+    # Prompt for environment name if not provided
+    if [ -z "$env_name" ]; then
+        read -p "Enter the environment name (default is base): " env_name
+        env_name="${env_name:-base}"  # Set default to 'base' if empty
+    fi
+
+    # Prompt for device if not provided
+    # Prompt for device if not provided
+    if [ -z "$device" ]; then
+        while true; do
+            read -p "Enter device type (cpu or cuda): " device
+            case "$device" in
+                "cpu" | "cuda")
+                    break  # Exit loop if device type is valid
+                    ;;
+                *)
+                    echo "Invalid device type. Please enter 'cpu' or 'cuda'."
+                    ;;
+            esac
+        done
+    fi
+}
+
 # Detect the operating system
 OS=$(uname)
-
 echo "${YELLOW}Detected OS: $OS ${RESET}"
 
 # Check if Conda is already installed
@@ -28,52 +65,75 @@ if command -v conda &> /dev/null; then
     echo "${YELLOW}Conda is already installed.${RESET}"
 else
   case "$(uname -s)" in
-    Linux*)
-    # Download and install Miniconda
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    chmod +x Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh
+      Linux*)
+      # Download and install Miniconda
+      wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+      chmod +x Miniconda3-latest-Linux-x86_64.sh
+      bash Miniconda3-latest-Linux-x86_64.sh
 
-    # Initialize Conda for the current shell
-    source $HOME/miniconda3/etc/profile.d/conda.sh
+      # Initialize Conda for the current shell
+      source $HOME/miniconda3/etc/profile.d/conda.sh
 
-    echo "${YELLOW}Initializing Conda for the default shell $(basename $SHELL) ${RESET}"
-    # Get the shell's configuration file
-    shell_config=""
+      echo "${YELLOW}Initializing Conda for the default shell $(basename $SHELL) ${RESET}"
+      # Get the shell's configuration file
+      shell_config=""
 
-    case "$(basename $SHELL)" in
-        bash) shell_config=~/.bashrc;;
-        zsh) shell_config=~/.zshrc;;
-        *)   echo "${RED}Shell type $(basename $SHELL) is not supported. Manual initialization may be required.${RESET}";;
-    esac
+      case "$(basename $SHELL)" in
+          bash) shell_config=~/.bashrc;;
+          zsh) shell_config=~/.zshrc;;
+          *)   echo "${RED}Shell type $(basename $SHELL) is not supported. Manual initialization may be required.${RESET}";;
+      esac
 
-    if [ -n "$shell_config" ]; then
-        # Initialize Conda for the restarted shell
-    ~/miniconda3/bin/conda init $(basename $SHELL)
+      if [ -n "$shell_config" ]; then
+          # Initialize Conda for the restarted shell
+      ~/miniconda3/bin/conda init $(basename $SHELL)
 
-# Display additional message
-    echo -e "${YELLOW} Conda has been automatically initialized in your terminal and you should see (base). if not, restart it manually and verify that it's inside your file shell_config.${RESET}"
-  fi
+      # Display additional message
+      echo -e "${YELLOW} Conda has been automatically initialized in your terminal and you should see (base). if not, restart it manually and verify that it's inside your file shell_config.${RESET}"
+      fi
 
-  ;;
+      ;;
   *)
     echo "${RED}Miniconda installation is only supported on Linux at the moment.${RESET}"
     ;;
   esac
 fi
 
-# Installing the environment
-echo "${YELLOW}Installing environment...${RESET}"
-cd requirements
-# ask if gpu is available
-if ask_yes_no "Do you have a GPU?"; then
-  conda env update -n gpu -f conda-env-gpu.yml
+# Installing the environment (true if non-interactive mode)
+if ask_yes_no "Do you want to install the environment ?"; then
+
+  cd requirements
+  # Parse command-line arguments
+  parse_arguments "$@"
+
+  # Perform the installation based on the chosen device
+  case "$device" in
+      cuda)
+          echo "Installing CUDA environment for $env_name"
+          # Install CUDA environment (assuming conda-env-gpu.yml contains CUDA dependencies)
+          conda env update -n "$env_name" -f conda-env-cuda.yml
+          ;;
+      cpu)
+          echo "Installing CPU environment for $env_name"
+          # Install CPU environment (assuming conda-env-cpu.yml contains CPU dependencies)
+          conda env update -n "$env_name" -f conda-env-cpu.yml
+          ;;
+      *)
+          echo "Unsupported device: $device"
+          exit 1
+          ;;
+  esac
+  cd ..
+  # restarting the terminal
+  echo "${YELLOW}Restarting the terminal...${RESET}"
+  exec $SHELL
+
 else
-    # install cpu environment
-  conda env update -n cpu -f conda-env-cpu.yml
+  echo "${YELLOW}Skipping the installation of the dev environment...${RESET}"
 fi
 
-pip install -r requirements-dev.txt
-# restarting the terminal
-echo "${YELLOW}Restarting the terminal...${RESET}"
-exec $SHELL
+# Initialize Conda for the current shell
+eval "$(conda shell.bash hook)"
+conda activate "$env_name"
+conda install pre-commit
+pre-commit install
